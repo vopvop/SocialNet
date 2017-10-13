33351eca8f298d0c1c6c283369bd4312b1e2a389
Maksim Sharonov <msharonov@users.noreply.github.com>
2017-10-13 21:47:37 +0700

    Add Identity API service

---
+++ src/services/identity/Veises.SocialNet.Identity/Models/CreateUserIdentity.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Veises.SocialNet.Identity.Models
+{
+	/// <summary>
+	/// New user identity model
+	/// </summary>
+	public sealed class CreateUserIdentity
+	{
+		/// <summary>
+		/// User name
+		/// </summary>
+		[Required]
+		public string UserName { get; set; }
+	}
+}
---
+++ src/services/identity/Veises.SocialNet.Identity/Startup.cs
@@ -0,0 +1,61 @@
+﻿using System.IO;
+
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.PlatformAbstractions;
+
+using Swashbuckle.AspNetCore.Swagger;
+
+namespace Veises.SocialNet.Identity
+{
+	public class Startup
+	{
+		public Startup(IConfiguration configuration)
+		{
+			Configuration = configuration;
+		}
+
+		public IConfiguration Configuration { get; }
+
+		// This method gets called by the runtime. Use this method to add services to the container.
+		public void ConfigureServices(IServiceCollection services)
+		{
+			services.AddMvc();
+
+			services.AddSwaggerGen(c =>
+			{
+				c.SwaggerDoc(
+					"v1",
+					new Info
+					{
+						Title = "Veises User Identity API",
+						Version = "v1",
+						Description = "User identity API service"
+					});
+
+				var basePath = PlatformServices.Default.Application.ApplicationBasePath;
+				var xmlPath = Path.Combine(basePath, "Veises.SocialNet.Identity.xml");
+				c.IncludeXmlComments(xmlPath);
+			});
+		}
+
+		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+		public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+		{
+			if (env.IsDevelopment())
+			{
+				app.UseDeveloperExceptionPage();
+			}
+
+			app.UseSwagger();
+			app.UseSwaggerUI(c =>
+			{
+				c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+			});
+
+			app.UseMvc();
+		}
+	}
+}
