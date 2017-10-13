4a3da3629729bddc3312537b247860f0e0b2a4fa
Maksim Sharonov <sharonovmi@gmail.com>
2017-10-13 21:47:37 +0700

    Add Identity API service

---
+++ src/services/identity/Veises.SocialNet.Identity/Controllers/IdentityController.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections.Generic;
+
+using Microsoft.AspNetCore.Mvc;
+
+using Veises.SocialNet.Identity.Models;
+
+namespace Veises.SocialNet.Identity.Controllers
+{
+	/// <summary>
+	/// User identity controller
+	/// </summary>
+	[Produces("application/json", "application/xml")]
+	[Route("api/Identity")]
+	public sealed class IdentityController: Controller
+	{
+		/// <summary>
+		/// Get all user identities
+		/// </summary>
+		/// <returns></returns>
+		[HttpGet]
+		public IEnumerable<UserIdentity> Get()
+		{
+			return new[] { new UserIdentity { UserName = "value1" }, new UserIdentity { UserName = "value2" } };
+		}
+
+		/// <summary>
+		/// Get specified user identity
+		/// </summary>
+		/// <param name="id"></param>
+		/// <returns></returns>
+		[HttpGet("{id}", Name = "Get")]
+		public string Get(int id)
+		{
+			return "value";
+		}
+
+		/// <summary>
+		/// Update user identity
+		/// </summary>
+		/// <param name="value"></param>
+		[HttpPost]
+		public void Post([FromBody]CreateUserIdentity createIdentityModel)
+		{
+		}
+
+		/// <summary>
+		/// Create user identity
+		/// </summary>
+		/// <param name="id"></param>
+		/// <param name="value"></param>
+		[HttpPut("{id}")]
+		public void Put(int id, [FromBody]string value)
+		{
+		}
+
+		/// <summary>
+		/// Delete user identity
+		/// </summary>
+		/// <param name="id"></param>
+		[HttpDelete("{id}")]
+		public void Delete(int id)
+		{
+		}
+	}
+}
---
+++ src/services/identity/Veises.SocialNet.Identity/Models/UserIdentity.cs
@@ -0,0 +1,13 @@
+﻿namespace Veises.SocialNet.Identity.Models
+{
+	/// <summary>
+	/// User identity model
+	/// </summary>
+	public sealed class UserIdentity
+	{
+		/// <summary>
+		/// User display name
+		/// </summary>
+		public string UserName { get; set; }
+	}
+}
