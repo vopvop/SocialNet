8e9b6a40f2716456eb7138e19144dec0c38df153
Maksim Sharonov <msharonov@users.noreply.github.com>
2017-10-17 23:04:59 +0700

    Add session support for the Frontend module

---
+++ src/frontend/Veises.SocialNet.Frontend/Controllers/IdentityController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Veises.SocialNet.Frontend.Controllers
+{
+	[Produces("application/json")]
+	[Route("api/Identity")]
+	public sealed class IdentityController: Controller
+	{
+		private const string userIdKey = "userUid";
+
+		/// <summary>
+		/// Get current user identity
+		/// </summary>
+		/// <returns></returns>
+		[HttpGet]
+		[Route("Current")]
+		[ProducesResponseType(200)]
+		[ProducesResponseType(401)]
+		[ProducesResponseType(404)]
+		public IActionResult GetCurrent()
+		{
+			// TODO: validate cookie and user
+
+			return NotFound();
+		}
+	}
+}
