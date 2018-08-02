3f398628f706c6b5ad3e29ec8e7ae595a4963d9e
Maksim Sharonov <msharonov@users.noreply.github.com>
2018-08-02 15:12:57 +0700

    Minor improvements.

--- src/services/example/Veises.Service.Example/Controllers/ValuesController.cs
+++ src/services/example/Veises.Service.Example/Controllers/ValuesController.cs
@@ -3,50 +3,73 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace Veises.Service.Example.Controllers
 {
+	/// <summary>
+	/// Default example Web API controller
+	/// </summary>
 	[ApiController]
 	[ApiVersion("1.0")]
 	[Route("api/v{version:apiVersion}/[controller]")]
 	[Produces("application/json", "application/xml")]
-	public class ValuesController : ControllerBase
-    {
+	public sealed class ValuesController : ControllerBase
+	{
 		/// <summary>
-		/// Get all values
+		/// Get all values.
 		/// </summary>
-		/// <returns></returns>
-        // GET api/values
-        [HttpGet(Name ="Get all values")]
-		[ProducesResponseType(200, Type =typeof(IEnumerable<string>))]
-        public IActionResult Get()
-        {
-            return Ok(new string[] { "value1", "value2" });
-        }
+		/// <returns>Value.</returns>
+		[HttpGet(Name = "Get all values")]
+		[ProducesResponseType(200, Type = typeof(IEnumerable<string>))]
+		public IActionResult Get()
+		{
+			return Ok(new string[] {"value1", "value2"});
+		}
 
-        // GET api/values/5
-        [HttpGet("{id}")]
-        public IActionResult Get(int id)
-        {
-            return Ok("value");
-        }
+		/// <summary>
+		/// Get value by ID
+		/// </summary>
+		/// <param name="id">Value Identifier</param>
+		/// <returns>Value</returns>
+		[HttpGet("{id}")]
+		[ProducesResponseType(200)]
+		public IActionResult Get(int id)
+		{
+			return Ok($"value{id}");
+		}
 
-        // POST api/values
-        [HttpPost]
-        public IActionResult Post([FromBody] string value)
-        {
+		/// <summary>
+		/// Add new value.
+		/// </summary>
+		/// <param name="value">Value to update.</param>
+		/// <returns>Operation result.</returns>
+		[ProducesResponseType(200)]
+		[HttpPost]
+		public IActionResult Post([FromBody] string value)
+		{
 			return Ok();
-        }
+		}
 
-        // PUT api/values/5
-        [HttpPut("{id}")]
-        public IActionResult Put(int id, [FromBody] string value)
-        {
+		/// <summary>
+		/// Update existing value. 
+		/// </summary>
+		/// <param name="id">Existing value identifier.</param>
+		/// <param name="value">New value.</param>
+		/// <returns>Operation result.</returns>
+		[ProducesResponseType(200)]
+		[HttpPut("{id}")]
+		public IActionResult Put(int id, [FromBody] string value)
+		{
 			return Ok();
-        }
+		}
 
-        // DELETE api/values/5
-        [HttpDelete("{id}")]
-        public IActionResult Delete(int id)
-        {
+		/// <summary>
+		/// Delete existing value.
+		/// </summary>
+		/// <param name="id">Existing value Identifier.</param>
+		/// <returns>Operation result.</returns>
+		[ProducesResponseType(200)]
+		[HttpDelete("{id}")]
+		public IActionResult Delete(int id)
+		{
 			return Ok();
-        }
-    }
+		}
+	}
 }
